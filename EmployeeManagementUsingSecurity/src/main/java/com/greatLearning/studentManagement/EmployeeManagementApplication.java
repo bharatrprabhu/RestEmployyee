7d80a28b4c86856a5037f4e65764a8853c43fcc6
bharat <107795998+bharatrprabhu@users.noreply.github.com>
2022-11-05 01:39:36 +0530

    Add files via upload

---
+++ EmployeeManagementUsingSecurity/src/main/java/com/greatLearning/studentManagement/EmployeeManagementApplication.java
@@ -0,0 +1,14 @@
+package com.greatLearning.studentManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeManagementApplication.class, args);
+		System.out.println("Employee Management System Started");
+	}
+
+}
